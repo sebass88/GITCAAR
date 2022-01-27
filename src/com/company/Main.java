29ee0b063d18a848e3d728de1134a90a1f05ce29
sebastian <sebassg8809@gmail.com>
2022-01-26 19:56:18 -0500

    the red car is ready for the trial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,14 @@ public class Main {
     public static void main(String[] args) {
         // the basic structure
         CarBasics car= new CarBasics();
+
+        car.color="red";
+        car.engine="7999cc";
+        car.door="2";
+        car.number="119018";
+
+        System.out.println(car.color);
+
+
     }
 }
