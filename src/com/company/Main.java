52274be9d3bcbcd14fbd9db3054c5e6e063948ce
sebastian <sebassg8809@gmail.com>
2022-01-26 20:18:54 -0500

    here is your car now dont ask me to make changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,14 +7,12 @@ public class Main {
     public static void main(String[] args) {
         // the basic structure
         CarBasics car= new CarBasics();
-
-        car.color="red";
-        car.engine="7999cc";
-        car.door="2";
-        car.number="119018";
+        car.color="green";
+        car.number="789";
+        car.engine="54444cc";
+        car.door="3";
 
         System.out.println(car.color);
-
 
     }
 }
